a5ccffd8bb4149084c34e715e4475ad6022814d4
MurroesWellbank <mw@mw.org>
2013-03-16 18:20:24 +0000

    Initial check in

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<?php
+wp_head();?>
+</head>
+<body>
+<?php 
+get_header();?>
+Welcome the to Murroes And Wellbank Community.
+<p /><?php bloginfo($name);?>
+
+<?php 
+get_footer();?>
+</body>
+</html>
